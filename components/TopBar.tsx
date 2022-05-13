8c262710159c77ebedba510bb09059b36c8b0808
ImInfenix <duncan.sourdin@pm.me>
2022-05-13 11:05:47 +0200

    Starting to style the topbar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -3,34 +3,16 @@ import styles from '../styles/TopBar.module.css'
 
 export default function TopBar() {
   return (
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <li className="menu-text">Swarm</li>
+    <div className={styles.topBar}>
+      <div className={styles.leftContent}>
+        <Link href="/">Swarm</Link>
+      </div>
+      <div className={styles.rightContent}>
+        <ul>
           <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/tickets">Tickets</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
+            <button type="submit">Sign in</button>
           </li>
         </ul>
-      </div>
-      <div className="top-bar-right">
-        <form action="#">
-          <ul className="menu">
-            <li>
-              <input type="search" placeholder="Search" />
-            </li>
-            <li>
-              <button type="submit" className="button">
-                Search
-              </button>
-            </li>
-          </ul>
-        </form>
       </div>
     </div>
   )
