add5acc1a27f58db7971c8faeb1e00b61c82e37d
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2022-05-19 19:24:49 +0200

    Created a basic logo for Swarm

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,11 +1,17 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/TopBar.module.css'
+import swarmLogo from "../public/SwarmLogo.svg"
 
 export default function TopBar() {
   return (
     <div className={styles.topBar}>
       <div className={styles.leftContent}>
-        <Link href="/">Swarm</Link>
+        <Link href="/">
+          <a>
+            <Image src={swarmLogo} alt="Swarm Logo" width={32} height={32} />
+          </a>
+        </Link>
       </div>
       <div className={styles.rightContent}>
         <ul>
