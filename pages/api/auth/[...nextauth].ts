e2e4a0b7c7a4cdf91b46fa3811b9df16dc7f7d13
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2022-06-06 17:30:58 +0200

    Configured NextAuth to return the userId

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth"
+import NextAuth, { Account, Profile } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "../../../lib/prisma"
+import { JWT } from "next-auth/jwt"
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -11,4 +12,11 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  session: {strategy: "database"},
+  callbacks: {
+    session: async ({session, user, token}) => {
+       session.id = user.id
+      return session
+    }
+  }
 })
